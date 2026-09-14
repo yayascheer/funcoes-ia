09dc74f03d598faac1e2bc67bcc8c32fe47c7681
yayascheer <yasmin.scheer@escola.pr.gov.br>
2026-09-14 13:42:31 +0000

    Aula 04

--- main.js
+++ main.js
@@ -50,6 +50,13 @@ let perguntaAtual;
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
 }
-
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
 mostraPergunta();
